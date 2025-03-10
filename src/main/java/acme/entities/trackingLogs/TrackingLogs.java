934b470399ab5d2c43126e7d869039070de501ac
JLF1828 <rafaberalv@gmail.com>
2025-03-10 02:09:09 +0100

    TrackingLogs creados y ClaimType subido

---
+++ src/main/java/acme/entities/trackingLogs/TrackingLogs.java
@@ -0,0 +1,53 @@
+
+package acme.entities.trackingLogs;
+
+import java.util.Date;
+
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.claim.Claim;
+
+public class TrackingLogs extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@OneToOne
+	private Claim				claim;
+
+	@Mandatory
+	@ValidMoment
+	@Automapped
+	private Date				lastUpdateMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				step;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 100)
+	@Automapped
+	private Double				resolutionPercentage;
+
+	@Mandatory
+	@Automapped
+	private boolean				indicator;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				resolution;
+}
