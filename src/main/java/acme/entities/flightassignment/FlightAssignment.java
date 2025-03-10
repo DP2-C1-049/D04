6b42ef26e04152544c1ef43949213c6681fe1635
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2025-03-10 10:41:40 +0100

    Task-004-Student-3 FlightAssignment moment validator fix

--- src/main/java/acme/entities/flightassignment/FlightAssignment.java
+++ src/main/java/acme/entities/flightassignment/FlightAssignment.java
@@ -48,7 +48,7 @@ public class FlightAssignment extends AbstractEntity {
 	private Duty				duty;
 
 	@Mandatory
-	@ValidMoment(past = true, min = "2000/01/01 00:00:00")
+	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				moment;
 
