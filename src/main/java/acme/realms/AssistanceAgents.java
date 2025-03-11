f196f4cef65197ed8a19f1a2e6777fda35be2aa1
JLF1828 <rafaberalv@gmail.com>
2025-03-11 18:09:04 +0100

    Fixed date de Airline y descomentado airline de AssistanceAgent

--- src/main/java/acme/realms/AssistanceAgents.java
+++ src/main/java/acme/realms/AssistanceAgents.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -17,6 +19,7 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.entities.airline.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -41,16 +44,11 @@ public class AssistanceAgents extends AbstractRole {
 	@Automapped
 	private String				spokenLenguages;
 
-	/*
-	 * @Mandatory
-	 * 
-	 * @Valid
-	 * 
-	 * @ManyToOne(optional = false)
-	 * 
-	 * @Automapped
-	 * private Airline airline;
-	 */
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	@Automapped
+	private Airline				airline;
 
 	@Mandatory
 	@ValidMoment(past = true)
