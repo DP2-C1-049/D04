5bf47838866e0c7e896a9e745ceac96044f6ce2a
STX3837 <albertopaleteirolopez@gmail.com>
2025-03-11 16:23:50 +0100

    Entity flight created

---
+++ src/main/java/acme/entities/flight/Flight.java
@@ -0,0 +1,42 @@
+
+package acme.entities.flight;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Flight extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Automapped
+	@ValidString(max = 50)
+	private String				tag;
+
+	@Mandatory
+	@Automapped
+	@Valid
+	private Boolean				indication;
+
+	@Mandatory
+	@Automapped
+	@ValidMoney(min = 0)
+	private Money				cost;
+
+	@Mandatory
+	@Automapped
+	@ValidString
+	private String				description;
+}
