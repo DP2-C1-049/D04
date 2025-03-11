157f119accd713be095318516c35c668907456b4
sergiopons-1 <sergioponslopez@gmail.com>
2025-03-11 18:06:03 +0100

    Customers added to Booking entity

--- src/main/java/acme/entities/booking/Booking.java
+++ src/main/java/acme/entities/booking/Booking.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -15,6 +16,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.entities.customers.Customers;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,11 +32,11 @@ public class Booking extends AbstractEntity {
 	//Attributes --------------------------------------------------------------------------
 
 	//Commented until creating the consumer entity
-	//	@Mandatory
-	//	@Automapped
-	//	@Valid
-	//	@ManyToOne
-	//	private Customer customer;
+	@Mandatory
+	@Automapped
+	@Valid
+	@ManyToOne
+	private Customers			customers;
 
 	@Mandatory
 	@Column(unique = true)
