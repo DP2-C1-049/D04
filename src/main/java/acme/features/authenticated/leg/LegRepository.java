7a244b48a4f94247e317d7c896f9d0e186db4a2e
sergiopons-1 <sergioponslopez@gmail.com>
2025-05-13 16:47:57 +0200

    correccion de cosas de la clase del 29/04

--- src/main/java/acme/features/authenticated/leg/LegRepository.java
+++ src/main/java/acme/features/authenticated/leg/LegRepository.java
@@ -2,6 +2,7 @@
 package acme.features.authenticated.leg;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
@@ -27,4 +28,10 @@ public interface LegRepository extends AbstractRepository {
 
 	@Query("SELECT l.arrivalAirport.city FROM Leg l WHERE l.flight.id = :flightId AND l.arrival = (SELECT MAX(l2.arrival) FROM Leg l2 WHERE l2.flight.id = :flightId)")
 	Optional<String> findDestinationCity(@Param("flightId") int flightId);
+
+	@Query("SELECT l.departureAirport.city FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.departure ASC")
+	List<String> findOrderedOriginCities(@Param("flightId") int flightId);
+
+	@Query("SELECT l.arrivalAirport.city FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.arrival DESC")
+	List<String> findOrderedDestinationCities(@Param("flightId") int flightId);
 }
